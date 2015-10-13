2a0bfdbbfc1ae0947a8dfdc1c4cb07038100bf8d
alvinly <alvinly74@gmail.com>
2015-10-13 09:59:32 -0700

    install React

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-
-
+  namespace :api, defaults: {format: :json} do
+    # resources
+  end
   resources :users, only: [:new, :create]
   resource :session, only: [:new, :create, :destroy]
+  root "static_pages#root"
 end
